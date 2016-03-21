375a6d2c81e49464c4f34189e24fb4f6bbdb55f4
tyfoan <myonlygamess@gmail.com>
2016-03-22 01:27:07 +0200

    Task done.

---
+++ ResponseEvaluator/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Response
+    {
+        public int ResponseId { get; set; }
+        public string Url { get; set; }
+        public string ResponseTime { get; set; }
+    }
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Response response)
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ContactInner_1()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ContactInner_2()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
